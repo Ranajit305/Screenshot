c734f00e0c8a47fc21bbabbb70ed3167f6a04220
Ranajit Nath <nathranajit6@gmail.com>
2025-04-02 16:57:50 +0530

    backend update

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,26 @@ import express from "express";
 import cors from "cors";
 import path from 'path';
 import 'dotenv/config'
+import helmet from 'helmet';
 
 import screenshotRouter from './routes/screenshot.route.js'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                imgSrc: ["'self'", "data:", "https://screenshot-r9na.onrender.com"],
+                scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+                styleSrc: ["'self'", "'unsafe-inline'"],
+                connectSrc: ["'self'", "https://screenshot-r9na.onrender.com"],
+            },
+        },
+    })
+);
 
 app.use("/api", screenshotRouter);
 
