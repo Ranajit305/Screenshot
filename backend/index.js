ac0de394ca739b5edec1419f48b091ec156c0885
Ranajit Nath <nathranajit6@gmail.com>
2025-04-02 17:20:55 +0530

    backend update

--- backend/index.js
+++ backend/index.js
@@ -13,21 +13,22 @@ app.use(
     helmet({
         contentSecurityPolicy: {
             directives: {
-                defaultSrc: ["'self'"],
-                imgSrc: ["'self'", "data:", "https://screenshot-r9na.onrender.com"],
-                scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
-                styleSrc: ["'self'", "'unsafe-inline'"],
-                connectSrc: ["'self'", "https://screenshot-r9na.onrender.com"],
+                defaultSrc: ["'self'", "*"],
+                imgSrc: ["'self'", "data:", "*"],
+                scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "*"],
+                styleSrc: ["'self'", "'unsafe-inline'", "*"],
+                connectSrc: ["'self'", "*"],
             },
         },
     })
 );
 
+
 app.use("/api", screenshotRouter);
 
 const __dirname = path.resolve();
 
-if (process.env.NODE === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
